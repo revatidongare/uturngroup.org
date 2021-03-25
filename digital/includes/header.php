97f537b1fb5a2c6b414db89c0849185ee2e64e05
pallavitambe1234 <56679787+pallavitambe1234@users.noreply.github.com>
2021-03-25 17:51:44 +0530

    25/03/2021
    update

--- digital/includes/header.php
+++ digital/includes/header.php
@@ -36,35 +36,32 @@
                         </li>       -->  
                          <li><a href="team.php">Team</a></li>           
                         <li><a href="contact.php">Contact Us</a></li>
-                        <li  class="blink" >
-                                        <a href="HR/index.php"style="color: #046c79">
+                        
+                    </ul>
+                </div>                    
+            </nav>
+            <div class="link-button">
+              
+               <li  class="blink" >
+                                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="20" height="20"
+viewBox="0 0 172 172"
+style=" fill:#26e07f;"><defs><linearGradient x1="78.59325" y1="109.78975" x2="88.3005" y2="159.53358" gradientUnits="userSpaceOnUse" id="color-1_f1GMSGZvB21U_gr1"><stop offset="0" stop-color="#1abc9c"></stop><stop offset="1" stop-color="#16979d"></stop></linearGradient><linearGradient x1="117.95258" y1="71.66667" x2="122.73275" y2="71.66667" gradientUnits="userSpaceOnUse" id="color-2_f1GMSGZvB21U_gr2"><stop offset="0" stop-color="#c48f0c"></stop><stop offset="0.251" stop-color="#d19b16"></stop><stop offset="0.619" stop-color="#dca51f"></stop><stop offset="1" stop-color="#e0a922"></stop></linearGradient><linearGradient x1="54.04742" y1="71.66667" x2="49.26725" y2="71.66667" gradientUnits="userSpaceOnUse" id="color-3_f1GMSGZvB21U_gr3"><stop offset="0" stop-color="#c48f0c"></stop><stop offset="0.251" stop-color="#d19b16"></stop><stop offset="0.619" stop-color="#dca51f"></stop><stop offset="1" stop-color="#e0a922"></stop></linearGradient><radialGradient cx="86.00095" cy="67.00816" r="38.61643" gradientUnits="userSpaceOnUse" id="color-4_f1GMSGZvB21U_gr4"><stop offset="0" stop-color="#ffcf54"></stop><stop offset="0.261" stop-color="#fdcb4d"></stop><stop offset="0.639" stop-color="#f7c13a"></stop><stop offset="1" stop-color="#f0b421"></stop></radialGradient><radialGradient cx="55.34853" cy="23.87742" r="91.34441" gradientUnits="userSpaceOnUse" id="color-5_f1GMSGZvB21U_gr5"><stop offset="0" stop-color="#c26715"></stop><stop offset="0.508" stop-color="#b85515"></stop><stop offset="1" stop-color="#ad3f16"></stop></radialGradient><radialGradient cx="80.86082" cy="29.99338" r="100.89898" gradientUnits="userSpaceOnUse" id="color-6_f1GMSGZvB21U_gr6"><stop offset="0" stop-color="#c26715"></stop><stop offset="0.508" stop-color="#b85515"></stop><stop offset="1" stop-color="#ad3f16"></stop></radialGradient><linearGradient x1="145.125" y1="25.542" x2="145.125" y2="85.43025" gradientUnits="userSpaceOnUse" id="color-7_f1GMSGZvB21U_gr7"><stop offset="0" stop-color="#33bef0"></stop><stop offset="1" stop-color="#0a85d9"></stop></linearGradient></defs><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g><path d="M86,109.54608c-31.66592,0 -57.33333,17.63358 -57.33333,36.85817v4.09575c0,3.95958 3.20708,7.16667 7.16667,7.16667h100.33333c3.95958,0 7.16667,-3.20708 7.16667,-7.16667v-4.09575c0,-19.22458 -25.66742,-36.85817 -57.33333,-36.85817z" fill="url(#color-1_f1GMSGZvB21U_gr1)"></path><path d="M103.91667,118.25c0,6.79758 -10.14083,14.33333 -17.91667,14.33333c-7.77583,0 -17.91667,-7.53575 -17.91667,-14.33333c0,-9.44925 0,-25.20875 0,-25.20875h35.83333c0,0 0,15.7595 0,25.20875z" fill="#d6a121"></path><path d="M117.949,64.5h-1.88842v14.33333h2.51908c1.84542,0 3.4185,-1.118 3.73025,-2.65167l1.23267,-6.10242c0.59125,-2.91683 -2.07833,-5.57925 -5.59358,-5.57925z" fill="url(#color-2_f1GMSGZvB21U_gr2)"></path><path d="M54.051,64.5h1.88842v14.33333h-2.51908c-1.84542,0 -3.4185,-1.118 -3.73025,-2.65167l-1.23267,-6.10242c-0.59125,-2.91683 2.07833,-5.57925 5.59358,-5.57925z" fill="url(#color-3_f1GMSGZvB21U_gr3)"></path><path d="M86,22.93333c-15.91358,0 -32.25,2.41875 -32.25,36.81875c0,2.752 0,21.05925 0,24.00117c0,12.28008 19.35,27.33008 32.25,27.33008c12.9,0 32.25,-15.05 32.25,-27.33008c0,-2.94192 0,-21.24558 0,-24.00117c0,-34.4 -16.33642,-36.81875 -32.25,-36.81875z" fill="url(#color-4_f1GMSGZvB21U_gr4)"></path><path d="M86,19.86958c-15.60183,0 -17.91667,5.86233 -17.91667,5.86233c-6.3425,0.93525 -17.91667,10.22683 -17.91667,20.84783c0,5.92683 0.94958,10.30567 3.58333,25.08333c1.95292,-23.45292 8.05892,-32.25 14.33333,-32.25c4.54008,0 8.14492,3.58333 17.91667,3.58333c8.25242,0 10.30208,-3.58333 17.91667,-3.58333c10.75,0 14.33333,28.552 14.33333,32.25c2.15358,-10.78583 3.58333,-19.90542 3.58333,-25.08333c0,-12.28725 -13.58442,-26.71017 -35.83333,-26.71017z" fill="url(#color-5_f1GMSGZvB21U_gr5)"></path><path d="M86.78475,17.91667c-14.921,0 -18.69067,7.81883 -18.69067,7.81883c-0.1505,4.15308 1.8705,7.81883 1.8705,7.81883c0,0 2.72333,5.86233 18.275,5.86233c9.38833,0 22.84375,-5.85875 22.84375,-21.5c0,0 -3.02075,0 -24.29858,0z" fill="url(#color-6_f1GMSGZvB21U_gr6)"></path><path d="M168.41667,25.08333h-46.58333c-1.978,0 -3.58333,1.60533 -3.58333,3.58333v51.59283c0,1.77375 2.35067,2.40083 3.23575,0.86717l7.51425,-13.04333h39.41667c1.978,0 3.58333,-1.60533 3.58333,-3.58333v-35.83333c0,-1.978 -1.60533,-3.58333 -3.58333,-3.58333z" fill="url(#color-7_f1GMSGZvB21U_gr7)"></path></g></g></svg><a href="../HR/index.php"style="color: #046c79" target="_blank">
                                      Recruitment Consultancy
                                         </a>
                                        
 
                                     </li>
                                     <li  class="blink" >
-                                        <a href="c2c/index.php"style="color: #046c79">
+                                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="20" height="20"
+viewBox="0 0 172 172"
+style=" fill:#26e07f;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#1abc9c"><path d="M86,10.105l-86,43.3225l6.88,3.99765v49.85985c-3.98155,1.43536 -6.88,5.22533 -6.88,9.675c0,1.98842 0.63672,3.47877 1.36391,5.13313c0.72719,1.65435 1.63991,3.33913 2.53969,4.89125c1.79956,3.10425 3.58781,5.69078 3.58781,5.69078l2.82859,4.10516l2.82859,-4.10516c0,0 1.78825,-2.58654 3.58781,-5.69078c0.89978,-1.55212 1.8125,-3.2369 2.53969,-4.89125c0.72719,-1.65435 1.36391,-3.1447 1.36391,-5.13313c0,-4.44967 -2.89845,-8.23964 -6.88,-9.675v-45.86219l72.24,41.99219l86,-49.9875zM38.52531,83.59469c-8.3936,14.5168 -11.14291,31.12931 -11.00531,54.00531c29.7216,0 51.6,8.6 58.48,20.64c6.88,-12.04 28.7584,-20.64 58.48,-20.64c0.1376,-22.876 -2.61171,-39.48851 -11.00531,-54.00531l-43.9339,25.52453c-1.1008,0.6536 -2.30238,0.96078 -3.54078,0.96078c-1.2384,0 -2.43998,-0.30718 -3.54078,-0.96078zM10.32,113.52c1.90764,0 3.44,1.53237 3.44,3.44c0,-0.09642 -0.22328,1.0846 -0.78609,2.365c-0.56281,1.2804 -1.37009,2.79106 -2.19031,4.20594c-0.23146,0.39927 -0.23623,0.36232 -0.46359,0.73906c-0.22736,-0.37674 -0.23213,-0.3398 -0.46359,-0.73906c-0.82022,-1.41488 -1.6275,-2.92554 -2.19031,-4.20594c-0.56281,-1.2804 -0.78609,-2.46142 -0.78609,-2.365c0,-1.90763 1.53236,-3.44 3.44,-3.44z"></path></g></g></svg><a href="../c2c/index.html"style="color: #046c79" target="_blank">
                                      Campus to Career
                                         </a>
                                        
 
                                     </li>
-                    </ul>
-                </div>                    
-            </nav>
-            <div class="link-button">
-               <!--  <div class="search_option">
-
-                    <a href="#"><span class="icon fa fa-search" style="color: black" ></span></a>
-                </div> -->
-                <ul class="social-links">
-                    <li><a href="http://facebook.com/vaibhav.k.123276/" target="_blank"><i class="fa fa-facebook" aria-hidden="true" style="color:black" ></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true" style="color: black" ></i></a></li>
-                    
-                    <li><a href="mailto:contact@uturngroup.org"><i class="fa fa-google-plus" aria-hidden="true" style="color: black" ></i></a></li>
-                    
-                </ul>
             </div>
         </div>
     </header>
