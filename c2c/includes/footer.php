840a4f41e96874bb15b02387dfe7e99081fd2dec
CoderBee <revatid15@gmail.com>
2021-04-06 18:45:56 +0530

    new changes

---
+++ c2c/includes/footer.php
@@ -0,0 +1,47 @@
+<footer>
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-3 col-sm-3">
+                    <p id="logo_footer">
+                        <img src="img/logo.jpg" width="50%" alt="Atena" data-retina="true">
+                    </p>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <h4>About</h4>
+                    <ul>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
+                        <li><a href="#">Terms and condition</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <h4>Academic</h4>
+                    <ul>
+                        <li><a href="#">Plans of study</a></li>
+                        <li><a href="#">Courses</a></li>
+                        <li><a href="#">Admissions</a></li>
+                        <li><a href="#">Staff</a></li>
+                        <li><a href="#">Students</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <h4>Contact us</h4>
+                    <ul>
+                        <li><a href="#">Contacts</a></li>
+                        <li><a href="#">Plan a visit</a></li>
+                    </ul>
+                    <ul id="contacts_footer">
+                        <li>Info line - <a href="tel://9579442271">+91 8766974657 / 9579442271</a></li>
+                        <li>Email - <a href="#">contact@uturngroup.org</a> </li>
+                    </ul>
+                </div>
+            </div><!-- End row -->
+        </div><!-- End container -->
+        </footer><!-- End footer -->
+        <div id="copy">
+            <div class="container">
+                 © Atena 2015 - All rights reserved.
+            </div>
+        </div><!-- End copy -->
